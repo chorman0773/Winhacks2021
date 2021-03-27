335049be79c8a5391ead88b277ae4d6b79335859
Ray Redondo <rdrpenguin04@gmail.com>
2021-03-26 23:57:08 -0500

    We have content! Kinda! Sorta!

--- app/src/main/java/xyz/lcdev/winhacks2021/SimpleEndpoint.java
+++ app/src/main/java/xyz/lcdev/winhacks2021/SimpleEndpoint.java
@@ -1,12 +1,14 @@
 package xyz.lcdev.winhacks2021;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class SimpleEndpoint {
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @GetMapping("/")
+    public String index(Model model) {
+        System.out.println("Meep");
+        return "index";
     }
 }
