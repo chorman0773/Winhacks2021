f64b2ee21378aed31846e65f36429889297503b8
Ray Redondo <rdrpenguin04@gmail.com>
2021-03-26 22:06:38 -0500

    Stub YAY

---
+++ app/src/main/java/xyz/lcdev/winhacks2021/Database.java
@@ -0,0 +1,23 @@
+package xyz.lcdev.winhacks2021;
+
+public class Database {
+    public Database() {
+        // Stub
+    }
+
+    public void setInt(String tableName, String key, int value) {
+        // Stub
+    }
+
+    public void setString(String tableName, String key, String value) {
+        // Stub
+    }
+
+    public int getInt(String tableName, String key) {
+        return 0; // Stub
+    }
+
+    public String getString(String tableName, String key) {
+        return ""; // Stub
+    }
+}
