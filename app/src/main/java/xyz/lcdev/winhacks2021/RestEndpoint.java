14222b11ea87ab19f211b14b3b9c04bfa7e0276d
Connor <chorman64@gmail.com>
2021-03-27 23:42:09 -0400

    Connect rest endpoints to the database

--- app/src/main/java/xyz/lcdev/winhacks2021/RestEndpoint.java
+++ app/src/main/java/xyz/lcdev/winhacks2021/RestEndpoint.java
@@ -7,21 +7,43 @@ import org.springframework.web.bind.annotation.RestController;
 import xyz.lcdev.winhacks2021.auth.User;
 
 import java.security.InvalidKeyException;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class RestEndpoint {
-    @GetMapping("/rest/getFacilities")
-    public String getFacilities() {
-        return "[{\"name\":\"Test site\",\"image\":\"/images/unknownSite.png\"},{\"name\":\"Test site 2\"}]";
+    @GetMapping(value="/rest/getFacilities",produces="application/json")
+    public List<Facility> getFacilities() throws SQLException {
+        Connection conn = Database.open();
+        var stmt = conn.prepareStatement("SELECT * FROM Facilities");
+        List<Facility> ret = new ArrayList<>();
+        try(var rs = stmt.executeQuery()){
+            while(rs.next()){
+                var id = UUID.fromString(rs.getString(1));
+                var name = rs.getString(2);
+                var location = rs.getString(3);
+                ret.add(new Facility(id,name,location));
+            }
+        }
+        return ret;
     }
 
     @GetMapping("/rest/getFacilityData")
-    public String getFacilityData(@RequestParam String name) {
-        return "{\"name\":\"Test site\",\"image\":\"SOMEONE GET ME A PLACEHOLDER\",\"location\":\"Smallville\"," +
-                "\"equipment\":[" +
-                "{\"name\":\"Centrifuge\",\"trained\":20,\"researchersUsing\":1,\"publications\":50,\"students\":2,\"samplesProcessed\":30}," +
-                "{\"name\":\"Fluorometer\",\"trained\":2,\"researchersUsing\":3,\"publications\":0,\"students\":0,\"samplesProcessed\":100}]}";
+    public List<FacilityRow> getFacilityData(@RequestParam String id) throws SQLException {
+        Connection conn = Database.open();
+        id+=".Equipment";
+        var stmt = conn.prepareStatement("SELECT * FROM ?");
+        stmt.setString(1,id);
+        List<FacilityRow> ret = new ArrayList<>();
+        try(var rs = stmt.executeQuery()){
+            while(rs.next()){
+                ret.add(new FacilityRow(id.getString(1),id.getLong(2),id.getLong(3),id.getLong(4),id.getLong(5),id.getLong(6)));
+            }
+        }
+        return ret;
     }
 
     @PostMapping(value="/auth/create-account",produces="application/json")
@@ -32,5 +54,71 @@ public class RestEndpoint {
     @PostMapping(value="/auth/lookup")
     public User lookupAccount(@RequestParam("email") String addr) throws SQLException {
         return User.getByAddress(addr);
+    }
+
+    static class Facility {
+        private final UUID id;
+        private final String name;
+        private final String location;
+
+        Facility(UUID id, String name, String location) {
+            this.id = id;
+            this.name = name;
+            this.location = location;
+        }
+
+        public UUID getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+    }
+
+    static class FacilityRow{
+        private final String name;
+        private final long trained;
+        private final long researchers;
+        private final long publications;
+        private final long students;
+        private final long samples;
+
+        FacilityRow(String name, long trained, long researchers, long publications, long students, long samples) {
+            this.name = name;
+            this.trained = trained;
+            this.researchers = researchers;
+            this.publications = publications;
+            this.students = students;
+            this.samples = samples;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public long getTrained() {
+            return trained;
+        }
+
+        public long getResearchers() {
+            return researchers;
+        }
+
+        public long getPublications() {
+            return publications;
+        }
+
+        public long getStudents() {
+            return students;
+        }
+
+        public long getSamples() {
+            return samples;
+        }
     }
 }
